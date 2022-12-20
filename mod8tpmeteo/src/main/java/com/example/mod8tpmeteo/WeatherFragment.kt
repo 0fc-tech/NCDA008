88c8f4f0bb44344a2ccc0abf8db2ff54dbb9ff44
qrouet <quentinrouet@hotmail.fr>
2022-12-20 09:47:37 +0100

    TP Meteo Correction Fin

--- mod8tpmeteo/src/main/java/com/example/mod8tpmeteo/WeatherFragment.kt
+++ mod8tpmeteo/src/main/java/com/example/mod8tpmeteo/WeatherFragment.kt
@@ -4,56 +4,58 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.findViewTreeLifecycleOwner
+import androidx.navigation.Navigation
+import com.example.mod8tpmeteo.databinding.FragmentWeatherBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [WeatherFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class WeatherFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    //TODO var pour dataBinding & ViewModel
+    lateinit var binding: FragmentWeatherBinding
+    lateinit var viewModel: WeatherViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weather, container, false)
+        //TODO Récupérer un dataBinding puis renvoyer le layout racine (constraintLayout)
+        binding = FragmentWeatherBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment WeatherFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            WeatherFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[WeatherViewModel::class.java]
+        binding.vm = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        //TODO répondre au clic des boutons et aller chercher température
+        binding.buttonLyon.setOnClickListener {
+            viewModel.getTemperature(viewModel.listCity.get("Lyon")!!)
+        }
+        binding.buttonLille.setOnClickListener {
+            viewModel.getTemperature(viewModel.listCity.get("Lille")!!)
+        }
+        binding.buttonMarseille.setOnClickListener {
+            viewModel.getTemperature(viewModel.listCity.get("Marseille")!!)
+        }
+        binding.buttonNiort.setOnClickListener {
+            viewModel.getTemperature(viewModel.listCity.get("Niort")!!)
+        }
+        binding.buttonNantes.setOnClickListener {
+            viewModel.getTemperature(viewModel.listCity.get("Nantes")!!)
+        }
+        binding.buttonRennes.setOnClickListener {
+            viewModel.getTemperature(viewModel.listCity.get("Rennes")!!)
+        }
+        binding.textViewCity.setOnClickListener {
+            viewModel.city.value?.let {
+                    city ->
+                val direction = WeatherFragmentDirections.actionWeatherToCity(city)
+                Navigation.findNavController(it).navigate(direction)
             }
+        }
     }
+
 }
--- mod8tpmeteo/src/main/java/com/example/mod8tpmeteo/WeatherViewModel.kt
+++ mod8tpmeteo/src/main/java/com/example/mod8tpmeteo/WeatherViewModel.kt
@@ -4,10 +4,30 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class WeatherViewModel: ViewModel() {
-    val city=MutableLiveData<City>()
+    val listCity = mapOf (
+        "Niort" to City("Niort", "79000", "Nouvelle Aquitaine"),
+        "Rennes" to City("Rennes", "35000", "Bretagne"),
+        "Lille" to City("Lille", "59000", "Haut de France"),
+        "Marseille" to City("Marseille", "13000", "PACA"),
+        "Lyon" to City("Lyon", "69000", "Auvergne Rhones Alpes"),
+        "Nantes" to City("Nantes", "44000", "Pays de la Loire"),
+    )
+    val city = MutableLiveData<City>()
     val temperature= MutableLiveData<Int>(0)
     init{
         city.value = City("","0","")
     }
+    fun getTemperature(city:City){
+        this.city.value = city
+        temperature.value = when(city.city){
+            "Niort"-> (-5..25).random()
+            "Rennes"->  (-10..20).random()
+            "Lille"-> (-10..10).random()
+            "Marseille"-> (0..40).random()
+            "Lyon"-> (0..35).random()
+            "Nantes"->  (-10..20).random()
+            else -> (-10..20).random()
+        }
 
+    }
 }
